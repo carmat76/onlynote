3ed6799d23abb15ea1a264cc93223c431984ab58
Your NameMatt <matt@nortide.com>
2025-07-02 18:34:07 -0400

    fix: correct vite config for Vercel

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   define: {
-    'process.env': {}, // Prevents build errors from packages expecting process.env
+    'process.env': {}, // Prevents issues with packages expecting process.env
   },
 });
